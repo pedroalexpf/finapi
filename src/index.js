a4b78d13fcd5cf29ede281d255291cae51493576
Pedro Flores <pedroalex.pf@gmail.com>
2021-10-10 09:23:48 -0300

    Middleware Added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found"});
+    }
+    return next();
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
     
@@ -26,15 +38,8 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement", (request, response) =>{
-    const { cpf } = request.headers;
-
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if (!customer) {
-        return response.status(400).json({ error: "Customer not found"});
-    }
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) =>{
+    
     return response.json(customer.statement);
 });
 
